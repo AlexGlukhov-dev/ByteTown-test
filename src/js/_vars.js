40cd2b093a29ad9b698a02e5b3dc3408da5ae349
AlexGlukhov-dev <alexglukhov37@gmail.com>
2023-01-23 18:00:40 +0300

    Added animation to hero section

--- src/js/_vars.js
+++ src/js/_vars.js
@@ -4,3 +4,7 @@ export default {
   htmlEl: document.documentElement,
   bodyEl: document.body,
 }
+
+export const blockOne = document.getElementById('block-one');
+export const blockTwo = document.getElementById('block-two');
+export const blockThree = document.getElementById('block-three');
