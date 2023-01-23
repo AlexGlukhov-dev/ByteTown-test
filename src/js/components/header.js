0c821fa440cfa0358cddca2ca0b6254c96b088c6
AlexGlukhov-dev <alexglukhov37@gmail.com>
2023-01-23 14:14:08 +0300

    Added animation of changing the header when switching to the light section

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,42 +1,22 @@
-const progress = document.getElementById("progressbar");
-const rotated = document.getElementById("menu-button");
-const upBtn = document.querySelector(".btn-up");
+const header = document.querySelectorAll('.header')
+const rotated = document.querySelectorAll(".header__logo");
+const secondBlock = document.querySelector('.feedback')
+const headerHeight = 94
 
-let totalHeight = document.body.scrollHeight - window.innerHeight;
-// progress.style.height = '0';
+function setHeader () {
+  const scrollPoint = window.scrollY + headerHeight
 
-window.addEventListener("scroll", () => {
-  let progressHeight = (window.scrollY / totalHeight) * 74.32;
-  progress.style.height = progressHeight + "%";
-});
-
-const parallax = (e) => {
-document.querySelectorAll('.layer')
-  .forEach(layer => {
-    const speed = layer.getAttribute('data-speed')
-    const x = (window.innerWidth - e.pageX * speed) / 50
-    const y = (window.innerHeight - e.pageY * speed) / 50
-    layer.style.transform = `translateX(${x}px) translateY(${y}px)`
-  })
+  let blockPoint = headerHeight - (scrollPoint - secondBlock.offsetTop)
+  if (blockPoint <= 0) blockPoint = 0
+  if (scrollPoint > secondBlock.offsetTop) {
+    header[0].style = `max-height: ${blockPoint ? blockPoint - 8 : blockPoint}px; padding-bottom: 0;`
+  } else {
+    header[0].style = `max-height: ${headerHeight + 64}px;`
+  }
 }
-
-// if (window.matchMedia(("(min-width: 768px")).matches) {
-//   window.addEventListener("mousemove", parallax);
-// }
-window.addEventListener("mousemove", parallax);
 
 window.addEventListener("scroll", () => {
   let offset = window.scrollY * 0.2;
-
-  rotated.style.transform = `rotate(${offset}deg)`;
-
-  if (window.scrollY > 100) {
-    upBtn.classList.remove("visually-hidden")
-    upBtn.style.opacity = 1;
-  } else {
-    upBtn.classList.add("visually-hidden")
-    upBtn.style.opacity = 0;
-  }
+  rotated.forEach(rot => rot.style.transform = `rotate(${offset}deg)`)
+  window.requestAnimationFrame(setHeader)
 });
-
-
